7e7e6b409ba5e6075cfcde8641b894930b4b4567
yurrpt <4m_cat@proton.me>
2022-11-26 23:58:35 +0300

    hi

--- lib/feature/second_page/second_page.dart
+++ lib/feature/second_page/second_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
