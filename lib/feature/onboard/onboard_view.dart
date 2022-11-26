e7014e764adfe75ad01b4b1e65beb5f610bf6e60
pyroaxy <ar.in4n@yandex.com>
2022-11-26 23:21:06 +0300

    Stop using scrollibing onboard screens.

--- lib/feature/onboard/onboard_view.dart
+++ lib/feature/onboard/onboard_view.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 import '../../constants/color_constants.dart';
 import '../second_page/second_page.dart';
 
-class OnboardView extends StatelessWidget {
+class OnboardView extends StatefulWidget {
   const OnboardView({super.key});
+
+  @override
+  State<OnboardView> createState() => _OnboardViewState();
+}
+
+class _OnboardViewState extends State<OnboardView> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    var dataSource = 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
+    _controller = VideoPlayerController.network(dataSource)
+      ..initialize().then((_) {
+        _controller.play();
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          const Align(
+          Align(
             alignment: Alignment.center,
-            child: Text('video'),
+            child: _controller.value.isInitialized
+                ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  )
+                : Container(),
           ),
           Padding(
             padding: const EdgeInsets.only(bottom: 12),
